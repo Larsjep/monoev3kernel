888b70056f4cb89cba50387bb002c8fdd7c7eef6
gloomyandy <gloomyandy@users.sourceforge.net>
2013-10-14 01:50:50 +0200

    cleanup script

--- kernel/build_extra.sh
+++ kernel/build_extra.sh
@@ -24,17 +24,3 @@ cd ${BACKPORTS}
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- KLIB=../modules/lib/modules/2.6.33-rc4/ clean
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- KLIB=../modules/lib/modules/2.6.33-rc4/ 
 make ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- KLIB=../modules/lib/modules/2.6.33-rc4/ KMODDIR=kernel KMODPATH_ARG="INSTALL_MOD_PATH=../modules" install
-exit
-DEST=`echo ../modules/lib/modules/*/`
-DEST=`echo ../modules/lib/modules/*/kernel/`
-sudo mkdir -p $DEST/compat
-sudo cp compat/*.ko $DEST/compat
-sudo mkdir -p $DEST/net/mac80211
-sudo cp net/mac80211/*.ko $DEST/net/mac80211
-sudo mkdir -p $DEST/net/wireless
-sudo cp net/wireless/*.ko $DEST/net/wireless
-sudo mkdir -p $DEST/drivers/net/wireless/ath
-sudo cp drivers/net/wireless/ath/*.ko $DEST/drivers/net/wireless/ath
-sudo mkdir -p $DEST/drivers/net/wireless/ath/ath9k
-sudo cp drivers/net/wireless/ath/ath9k/*.ko $DEST/drivers/net/wireless/ath/ath9k
-
